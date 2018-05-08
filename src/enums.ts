51dea1465b7fb50a48068b1558b74d151c207a84
Keith <keithwhisper@gmail.com>
2018-05-08 15:01:17 +0800

    update: add debug scripts

--- src/enums.ts
+++
@@ -1,9 +0,0 @@
-export enum FileErrors {
-  NotFound = 'File Not Found',
-  Exist = 'File Exists',
-}
-
-export enum FileAction {
-  CREATE = 'create',
-  UPDATE = 'update',
-}
--- src/utils/logger.ts
+++
@@ -1,5 +0,0 @@
-import * as log4js from 'log4js'
-
-const logger = log4js.getLogger()
-logger.level = process.env.NODE_ENV === 'production' ? 'info' : 'debug'
-export default logger
